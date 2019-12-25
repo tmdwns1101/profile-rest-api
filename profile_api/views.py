622b94f5e2b0199e9f4de74c429f4edd16bdf30e
DESKTOP-SAJN8LR\PC <tmdwns1101@naver.com>
2019-12-25 14:29:09 +0900

    Add login API

--- profile_api/views.py
+++ profile_api/views.py
@@ -4,9 +4,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from profile_api import serializers
 from profile_api import models
 from profile_api import permissions
+
 
 class HelloApiView(APIView):
     """
@@ -130,3 +134,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,filters.OrderingFilter,)
+    search_fields = ('name', 'email',) #튜플은 , 넣어줘야함
+    ordering_fields = ['name'] 
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """
+    Handle create user authentication tokens
+    """
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+     
